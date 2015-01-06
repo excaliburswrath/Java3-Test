a7fe37fe0d45b4c0eba85c59559a569b0bd4d6f0
excaliburswrath <edgardo.e.castro@gmail.com>
2015-01-06 11:58:38 -0600

    Test App created to test github

---
+++ src/edu/austincc/main/App.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu.austincc.main;
+
+/**
+ * @author Owner
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World!!!");
+System.out.println("Hello, World Too!!!");
+	}
+
+}
